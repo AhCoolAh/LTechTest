fe8b2fc62e096c45f65cfc4b1be0ff842a001bd0
Pavel Golovin <wergpsg@gmail.com>
2022-11-08 16:51:34 +0300

    Initial Commit

---
+++ LTechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LTechTest
+//
+//  Created by Pavel21 on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
